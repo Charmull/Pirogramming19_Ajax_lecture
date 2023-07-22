c2504960cb2a400681f26db37cc6cfda8bb42e2d
Charmull <527wlals@gmail.com>
2023-07-22 15:51:47 +0900

    add ajax code (origin, fetch, axios)

--- main/views.py
+++ main/views.py
@@ -30,3 +30,25 @@ def post_new(request):
             'form': form,
         }
         return render(request, 'main/post_new.html', ctx)
+    
+
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def like_ajax(request):
+    req = json.loads(request.body)  # {id: 1, type: 'like'}
+    post_id = req['id']
+    button_type = req['type']
+
+    post = Post.objects.get(id=post_id)
+
+    if button_type == 'like':
+        post.like += 1
+    else:
+        post.dislike += 1
+    
+    post.save()
+
+    return JsonResponse({'id': post_id, 'type': button_type})
